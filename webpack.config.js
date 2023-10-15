34f7163af4824f1abd7a77950b208fd853bfcea5
matsui-k <osskil611@gmail.com>
2023-10-15 21:20:26 +0900

    style-loader, css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 /** @type {import('webpack').Configuration} */
 const config = {
   mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
 
 // 設定を CommnJS 形式でエクスポート
